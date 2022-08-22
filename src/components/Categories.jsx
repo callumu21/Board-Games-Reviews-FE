927a96cf63a84be634d66f5d2e8fb7708148093d
callumu21 <cmu_92@hotmail.co.uk>
2022-08-22 17:12:46 +0100

    Add routing for categories and filtering

---
+++ src/components/Categories.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "../utils/api";
+import { formatCategories } from "../utils/formattingFunctions";
+import { useNavigate } from "react-router-dom";
+
+export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getCategories().then(({ categories }) => {
+      setCategories(categories);
+    });
+  }, []);
+
+  return (
+    <form
+      className="category-filter"
+      onSubmit={(event) => {
+        event.preventDefault();
+        navigate(`/reviews/${category}`);
+        setCategory("");
+      }}
+    >
+      <select
+        onChange={(event) => {
+          setCategory(event.target.value);
+        }}
+        className="category-filter__select"
+        value={category}
+      >
+        <option value="" disabled>
+          Category filter
+        </option>
+        {categories.map((category) => {
+          return (
+            <option value={category.slug} key={category.slug}>
+              {formatCategories(category.slug)}
+            </option>
+          );
+        })}
+      </select>
+      <button className="category-filter__btn">Filter!</button>
+    </form>
+  );
+}
--- src/components/CategoryReviews.jsx
+++ src/components/CategoryReviews.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
-import { getReviews } from "../utils/api";
+import { useParams } from "react-router-dom";
+import { getReviewsByCategory } from "../utils/api";
 import ReviewCard from "./ReviewCard";
 import Categories from "./Categories";
 
-export default function Reviews() {
+export default function CategoryReviews() {
   const [reviews, setReviews] = useState([]);
+  const { category } = useParams();
 
   useEffect(() => {
-    getReviews().then(({ reviews }) => {
+    getReviewsByCategory(category).then(({ reviews }) => {
       setReviews(reviews);
     });
-  }, []);
+  }, [category]);
 
   return (
     <main>
       <Categories />
       <ul className="reviews">
         {reviews.map((review) => {
-          return <ReviewCard review={review} />;
+          return <ReviewCard review={review} key={review.title} />;
         })}
       </ul>
     </main>
